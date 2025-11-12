06cc8effa0e19dd6805258fbb75a38c0c54492ba
Bruno Silva <bruno.ms1silva@gmail.com>
2025-11-12 09:43:20 +0000

    Fix git status

---
+++ Liga Portugal Zone/src/main/java/com/lp/ligaportugalzone/LigaPortugalZoneApplication.java
@@ -0,0 +1,13 @@
+package com.lp.ligaportugalzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaPortugalZoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LigaPortugalZoneApplication.class, args);
+    }
+
+}
