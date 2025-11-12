100df636ea52794a7f889f49d7f70ad13b99762d
Bruno Silva <bruno.ms1silva@gmail.com>
2025-11-12 10:10:49 +0000

    First commit backend

---
+++ Backend/src/test/java/com/lp/ligaportugalzone/LigaPortugalZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lp.ligaportugalzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaPortugalZoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
