7b71b0bf9ffd062a977954e5214a1af22070df2a
Bruno Silva <bruno.ms1silva@gmail.com>
2026-01-24 18:18:42 +0000

    Add CrossOrigin annotation for CORS support

--- Backend/src/main/java/com/lp/ligaportugalzone/player/PlayerController.java
+++ Backend/src/main/java/com/lp/ligaportugalzone/player/PlayerController.java
@@ -3,6 +3,7 @@ package com.lp.ligaportugalzone.player;
 import com.lp.ligaportugalzone.util.SlugUtils;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/players")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class PlayerController {
     private final PlayerService playerService;
 
